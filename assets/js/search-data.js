98a010b0eb64f4193b28ebd2e94261290580ed0f
AtomAndAdam <113654260+AtomAndAdam@users.noreply.github.com>
2025-02-05 13:30:12 +0000

    Deploying to gh-pages from @ AtomAndAdam/al-folio-blog.github.io@577b9438d3c94b7cc9607c45a1625457527d0941 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -112,7 +112,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/al-folio-blog.github.io//feed.xml", "_blank");
+          window.open("/al-folio-blog.github.io/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
